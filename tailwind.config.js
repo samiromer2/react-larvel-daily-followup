78e71b2c18f34cd919e06920e46689a39e9be391
Samir Omer <samiromer2@yahoo.com>
2023-05-01 15:25:12 +0300

    first follow up free week

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require('tailwindcss/colors')
+ 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.jsx'],
+  theme: {
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    extend: { colors },
+  },
+  plugins: [],
+}
